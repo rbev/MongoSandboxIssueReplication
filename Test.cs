be67535d706d118834bd381c0e70b4ad53377aea
Rhys Bevilaqua <rhys.bevilaqua@vedi.io>
2024-11-19 10:14:25 +0800

    minimal replication of failure

---
+++ Test.cs
@@ -0,0 +1,15 @@
+﻿using MongoSandbox;
+using NUnit.Framework;
+
+namespace MongoSandboxIssueReplication;
+
+public class Test
+{
+    [Test]
+    public void TestConstructor()
+    {
+        var db = MongoRunner.Run();
+        
+        Assert.Pass("Wooooo, it worked");
+    }
+}
